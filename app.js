b013378cfafbdd2dc574a4cfe222a11cdc0b29c7
Jeff Bustraan <jeffbustraan@me.com>
2021-11-25 14:08:26 -0500

    Added Style Sheet, nav bar

---
+++ app.js
@@ -0,0 +1,18 @@
+let age = 32;
+let userName = 'Jeff';
+let hobbies = ['Sports', 'Photography', 'Web Design'];
+let job = {
+    title: 'Developer',
+    place: 'New York',
+    salary: 50000
+};
+age = 45;
+
+let adultYears;
+
+function calculateAdultYears() {
+    adultYears = age - 18;
+}
+
+let adultYears = age - 18;
+alert(adultYears);
